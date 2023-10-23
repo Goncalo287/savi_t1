9e212a90273f75beaa91ff9d2c0451df46625fb2
Goncalo287 <goncalo.anacleto@ua.pt>
2023-10-23 14:43:01 +0100

    Save named templates to list

--- main.py
+++ main.py
@@ -3,7 +3,21 @@
 import cv2
 import numpy as np
 import copy
+import tkinter as tk
+from tkinter import simpledialog
 
+
+def openInputWindow():
+    '''
+    Opens a window where the user can input text
+    Returns a string or None (if the user cancels)
+    '''
+
+    root = tk.Tk()
+    root.withdraw()
+    user_input = simpledialog.askstring('Set template name', 'Person name:')
+    root.destroy()
+    return user_input
 
 
 def main():
@@ -11,6 +25,8 @@ def main():
     # Initialization
     cap = cv2.VideoCapture(0)
     face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    templates = []  # TODO: open templates saved on disk (add image format to .gitignore)
+
 
     # Execution
     while cap.isOpened():
@@ -37,14 +53,25 @@ def main():
 
         # Keyboard inputs
         k = cv2.waitKey(1) & 0xFF
+
         if k == ord('q') or k == 27:    # Q or ESC to exit
             break
+
+        elif k == ord('s'):     # S to save current face as a template
+
+            if len(faces) > 0:
+                name = openInputWindow()    # Open dialog box to input person's name
+                if name is not None and len(name)>0:
+                    new_template = {'name': name, 'img': img_gray[y:y+w, x:x+w]}    # (x, y, w, h) from face detection
+                    templates.append(new_template)
+                    print('Template saved:', name)
+            else:
+                print('Error: no faces detected')
 
 
     # Destroy cv2 windows
     cap.release()
     cv2.destroyAllWindows()
-        
 
 
 if __name__ == '__main__':
